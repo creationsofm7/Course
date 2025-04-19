a5b80402b3ecbe67c7b16e99a3dd17d2859d30a5
Mudit Pandey <Muditpanday17@gmail.com>
2025-04-19 15:41:43 +0530

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://courseai.netlify.app/" // the base url of your auth server
+    baseURL: "https://courseai.netlify.app" // the base url of your auth server
 })
 
